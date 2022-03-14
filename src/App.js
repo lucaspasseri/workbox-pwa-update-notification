3388b5dbc697e8234d9d82ce771095feafe53b05
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2022-03-14 18:51:37 -0300

    refactor: cleanup

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import './App.css';
 
 
 import React, {  useEffect } from "react";// useState,
-//default imports...
 import { withSnackbar, useSnackbar } from "notistack";
-//import * as serviceWorker from "./serviceWorker";
-//import { Button } from "@material-ui/core";
 import Button from '@mui/material/Button';
 
 
@@ -14,44 +11,12 @@ import { Workbox } from "workbox-window";
 
 function App() {
 
-	const { enqueueSnackbar, closeSnackbar } = useSnackbar(); //, closeSnackbar
-
-	/* const [state, setState] = useState({
-		newVersionAvailable: false,
-		wb: {},
-	}) */
-
-	/* const onServiceWorkerUpdate = (wb) => {
-		console.log(wb);
-    setState({
-      //waitingWorker: wb && wb.waiting,
-			worker: wb,
-      newVersionAvailable: true,
-    });
-  }; */
-
+	const { enqueueSnackbar, closeSnackbar } = useSnackbar();
   
-
-  
-
 	useEffect(() => {
     if ("serviceWorker" in navigator) {
 
-			/* const updateServiceWorker = () => {
-				const { wb } = state;
-				
-				console.log("updateServiceWorker()");
-				console.log(wb);
-		
-				wb && wb.messageSkipWaiting();
-				//waitingWorker.messageSkipWaiting();
-		
-				
-				setState({ newVersionAvailable: false });
-				//window.location.reload();
-			}; */
-
-			const refreshAction = ({onAccept}) => { //render the snackbar button
+			const refreshAction = ({onAccept}) => { //render the snackbar buttons
 				return (
 					<>
 						<Button
@@ -78,13 +43,7 @@ function App() {
         if (!event.isUpdate) {
           // First-installed code goes here...
           console.log("New SW was installed!");
-          /* if (confirm(`A newer version is available! Click OK to refresh.`)) {
-            wb.messageSkipWaiting();
-            window.location.reload();
-          } */
-        } else {
-          console.log("There is a new version of sw!");
-        }
+        } 
       });
 
       wb.addEventListener("waiting", (event) => {
@@ -92,12 +51,8 @@ function App() {
           `A new service worker has installed, but it can't activate ` +
             `until all tabs running the current version have fully unloaded. `
         );
-        //console.log(event);
 
         if (event.isUpdate) {
-          //setRefreshAlert(true);
-            console.log("eventListener, event.isUpdate = true");
-
 						enqueueSnackbar("A new version was released!", {
 							persist: true,
 							variant: "warning",
@@ -108,30 +63,11 @@ function App() {
 								},
 							}),
 						});
-
-						//console.log(wb);
-						//onServiceWorkerUpdate(wb);
-            // wb.messageSkipWaiting();
-            // window.location.reload();
-            // setRefreshResponse(false);
-            // setRefreshAlert(false);
         }
       });
 
-			// console.log(wb.isUpdate);
-			// wb.isUpdate && wb.update(console.log("wb.update"));
 
-      wb.register(); //{ onUpdate: onServiceWorkerUpdate }, console.log("Registered")
-
-			//wb.update(onServiceWorkerUpdate(wb));
-			//wb.update(console.log(wb));
-
-			/* if (state.newVersionAvailable) //show snackbar with refresh button
-      enqueueSnackbar("A new version was released", {
-        persist: true,
-        variant: "success",
-        action: refreshAction(),
-      }); */
+      wb.register();
 
       const versionMessage = async () => {
         //return await wb.messageSW({ type: "GET_VERSION" });
@@ -140,20 +76,14 @@ function App() {
       };
 
 			versionMessage();
-    /*   try {
-        af().then((resp) => console.log("Service Worker version:", resp));
-        //
-      } catch (e) {
-        console.log(e);
-      } */
     }
 		
 
-  }, [enqueueSnackbar]); // , []
+  }, [enqueueSnackbar, closeSnackbar]);
 
 
 
-  return ( //render components
+  return ( 
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -174,30 +104,3 @@ function App() {
 }
 
 export default withSnackbar(App);  //uses the snackbar context
-
-
-
-
-/* class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      newVersionAvailable: false,
-      waitingWorker: {},
-    };
-  } */
-
-/*   componentDidMount = () => {
-    const { enqueueSnackbar } = this.props;
-    const { newVersionAvailable } = this.state;
-		if (process.env.NODE_ENV === 'production') {
-    	serviceWorker.register({ onUpdate: this.onServiceWorkerUpdate });
-		}
-
-    if (newVersionAvailable) //show snackbar with refresh button
-      enqueueSnackbar("A new version was released", {
-        persist: true,
-        variant: "success",
-        action: this.refreshAction(),
-      });
-  }; */
